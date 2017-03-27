b8d89e144c35d49d9ac995120a50817676d60d7b
MercuryHunter <ujonathanalp@gmail.com>
2017-03-27 20:34:41 +0200

    Modified Client Sender to send commands in plaintext (e.g. /command) or to instead append name if the user has one. A later change may be made to give the name “Default” if they don’t give a name.

--- src/client/Sender.java
+++ src/client/Sender.java
@@ -16,11 +16,17 @@ class Sender implements Runnable {
 	public void run() {
 		try {
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			
 			String toSend;
+			// Send all data that comes in from stdIn
 			while((toSend = stdIn.readLine()) != null) {
-				String sendString = name == "" ? toSend : name + ": " + toSend;
-				send.println(sendString);
-				//System.out.println(sendString);
+				// Don't append name if it's a command
+				if(toSend.charAt(0) == '/') send.println(toSend);
+				// Otherwise try to append name if it exists
+				else {
+					String sendString = name == "" ? toSend : name + ": " + toSend;
+					send.println(sendString);
+				}
 			}
 		}
 		catch(Exception x) {
