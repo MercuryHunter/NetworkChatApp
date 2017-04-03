95363b3c7556fcadcd420b85d6052efa3fee3a30
MercuryHunter <ujonathanalp@gmail.com>
2017-04-03 20:58:08 +0200

    Brought code into methods and added hopefully helpful comments.

--- src/client/Receiver.java
+++ src/client/Receiver.java
@@ -3,10 +3,11 @@ package client;
 import java.net.*;
 import java.io.*;
 
-class Receiver implements Runnable {
+public class Receiver implements Runnable {
 
 	BufferedReader receive;
-	String host;
+	String host; 
+	// Host is required for file transfers, which are started on receiving the appropriate message.
 
 	public Receiver(BufferedReader receive, String host) {
 		this.receive = receive;
@@ -14,40 +15,49 @@ class Receiver implements Runnable {
 	}
 
 	public void run() {
+		// Continuously receive input and deal with it as necessary.
 		try {
 			String input;
 			while((input = receive.readLine()) != null) {
-				if(input.startsWith("/beginfilesend")) {
-					// Check if this file was previously requested to be sent.
-					String[] args = input.split(" ");
-
-					String fileName = args[1];
-					int port = Integer.parseInt(args[2]);
-
-					System.out.println("Received send command for upload.");
-
-					Thread senderThread = new Thread(new FileSender(fileName, port, host));
-					System.out.println("Sending file: " + fileName);
-					senderThread.start();
-				}
-				else if(input.startsWith("/beginfilereceive")) {
-					String[] args = input.split(" ");
-
-					String fileName = args[1];
-					int port = Integer.parseInt(args[2]);
-					int fileSize = Integer.parseInt(args[3]);
-
-					System.out.println("Received file args for download.");
-
-					Thread receiverThread = new Thread(new FileReceiver(fileName, port, fileSize, host));
-					System.out.println("Receiving file: " + fileName);
-					receiverThread.start();
-				}
-				else System.out.println(input);
+				handleInput(input);
 			}
 		}
 		catch(Exception x) {
 			x.printStackTrace();
 		}
+	}
+
+	private void handleInput(String input) {
+		if(input.startsWith("/beginfilesend"))
+			handleFileSend(input);
+		else if(input.startsWith("/beginfilereceive"))
+			handleFileReceive(input);
+		else System.out.println(input);
+	}
+
+	private void handleFileSend(String input) {
+		// TODO: Check if this file was previously requested to be sent.
+		// Parse parameters
+		String[] args = input.split(" ");
+
+		String fileName = args[1];
+		int port = Integer.parseInt(args[2]);
+
+		// Begin thread to send file.
+		Thread senderThread = new Thread(new FileSender(fileName, port, host));
+		senderThread.start();
+	}
+
+	private void handleFileReceive(String input) {
+		// Parse parameters
+		String[] args = input.split(" ");
+
+		String fileName = args[1];
+		int port = Integer.parseInt(args[2]);
+		int fileSize = Integer.parseInt(args[3]);
+
+		// Begin thread to receive file.
+		Thread receiverThread = new Thread(new FileReceiver(fileName, port, fileSize, host));
+		receiverThread.start();
 	}
 }
