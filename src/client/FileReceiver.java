e84cdd6b670da9029e78cba625f2c6034df455d7
MercuryHunter <ujonathanalp@gmail.com>
2017-04-03 20:30:48 +0200

    Better error handling and commenting in FileReceiver

--- src/client/FileReceiver.java
+++ src/client/FileReceiver.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.*;
 import java.util.zip.*;
 
-class FileReceiver implements Runnable {
+public class FileReceiver implements Runnable {
 
 	String fileName, host;
 	int port, fileSize;
@@ -19,34 +19,44 @@ class FileReceiver implements Runnable {
 
 	public void run() {
 		try {
+			// Open data connection to server
+			System.out.println("Beginning file download.");
 			Socket dataSocket = new Socket(host, port);
-			System.out.println("Downloading file...");
-
-			// Receive File
+			
+			// Open compressed stream and stream to file output
 			GZIPInputStream input = new GZIPInputStream(dataSocket.getInputStream());
 			BufferedOutputStream fileOutput = new BufferedOutputStream(new FileOutputStream("download/" + fileName), fileSize);
 
+			// Read data into buffer and write to file
 			byte[] buffer = new byte[1024];
 			int count;
 			while ((count = input.read(buffer)) > 0) {
-				//debugPrintArrayOfBytes(buffer, "Read:");
 				fileOutput.write(buffer, 0, count);
 			}
 
-			// Write into file and close streams
+			// Close streams
 			input.close();
 			fileOutput.flush();
 			fileOutput.close();
 
 			System.out.printf("File %s downloaded. (%d bytes)\n", fileName, fileSize);
 		}
-		catch (Exception e) {
-			System.err.println("Something went wrong receiving from server.");
+		catch (UnknownHostException e) {
+			System.err.println("Unable to see host: " + host);
+		}
+		catch (FileNotFoundException e) {
+			System.err.println("Error writing file to disk.");
+		}
+		catch (IOException e) {
+			System.err.println("File could not be downloaded properly.");
+			System.err.println("File may be incomplete.");
+			System.err.println("For bug assistance, provide the following report:");
 			e.printStackTrace();
+			System.err.println("End of Report.");
 		}
 	}
 
-	// DEBUG CODE
+	// Debug code for file transfer
 	private void debugPrintArrayOfBytes(byte[] bytes, String message) {
 		System.out.println(message);
 		for(int i = 0; i < bytes.length; i++) {
